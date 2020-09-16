d53c0199c315df359a994005b7619a3f74bdbf31
gurunatha prasad <46220539+vgurunathaprasad@users.noreply.github.com>
2020-09-16 11:57:54 +0530

    Add files via upload

---
+++ js_arun_medium_prbs.js
@@ -0,0 +1,31 @@
+function addFive(a) {
+    return (a+5);
+}
+
+function getOpposite(a){
+    if(isNaN(a)) return a;
+    if(!!(a%1)) return a;
+    return (a*-1);
+}
+
+function toSeconds(mins){
+    return mins*60;
+}
+
+function toInteger(strn){
+    return Number(strn);
+}
+
+function nextNumber(i) {
+    return (i+1);
+}
+
+function getFirstElement(arr){
+    return arr[0];
+}
+
+function hoursToSeconds(h){
+    return (h*60*60);
+}
+
+fucntion 
